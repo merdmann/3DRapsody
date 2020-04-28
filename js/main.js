d22953b0639c56d9d61660b35cf7171b3b998c6c
Michael Erdmann <michaelslab@outlook.de>
2020-04-28 21:07:52 +0200

    Snyc; currently it is not showing anything even thought i am a working example from the documentation

---
+++ js/main.js
@@ -0,0 +1,33 @@
+import * as THREE from "../js/three.js";
+
+document.addEventListener('DOMContentLoaded', function evt(x) {
+  console.log('++++++++ App.alive ' + x );
+  
+  
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+  var geometry = new THREE.BoxGeometry();
+  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  var cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
+
+  camera.position.z = 5;
+
+  const animate = function () {
+    requestAnimationFrame( animate );
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+  };
+
+  animate();
+})
+
+
