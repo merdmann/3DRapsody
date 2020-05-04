92886c7e0869c8fe30e952ed289ff0bb4d034f46
Michael Erdmann <michaelslab@outlook.de>
2020-05-04 21:46:28 +0200

    Added a light source and a new geometric element
    But it is not yet shown in the intended canvas

--- js/main.js
+++ js/main.js
@@ -4,27 +4,31 @@ document.addEventListener('DOMContentLoaded', () => {
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+    var outputCanvas = document.getElementById("OutputCanvas")
+    console.log(outputCanvas);
+
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.clear
     document.body.appendChild(renderer.domElement);
 
-    var geometry = new THREE.BoxGeometry();
-    var material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+    var geometry = new THREE.DodecahedronGeometry(1, 0);
+    var material = new THREE.MeshPhongMaterial({ color: 0xffff00, shininess: 0.5 });
     var cube = new THREE.Mesh(geometry, material);
 
     scene.add(cube);
     camera.position.z = 5;
     var spotLight = new THREE.SpotLight(0xffffff);
-    spotLight.position.set(100, 1000, 100);
+    spotLight.position.set(100, 1000, 100)
 
-    spotLight.castShadow = true;
+    spotLight.castShadow = true
     scene.add(spotLight)
 
     const animate = function() {
         requestAnimationFrame(animate);
 
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+        cube.rotation.x += 0.01
+        cube.rotation.y += 0.01
 
         renderer.render(scene, camera);
     };
